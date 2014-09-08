42a93b0fb3357d77f6133e5a75d7747334a4a390
Jake Nations <jnations@8thlight.com>
2014-09-08 15:45:13 -0500

    Removed duplication in  rules tests

--- src/test/RulesTest.java
+++ src/test/RulesTest.java
@@ -2,6 +2,8 @@ package test;
 
 import main.Board;
 import main.Rules;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -9,34 +11,39 @@ import static org.junit.Assert.*;
  * Created by Nayshins on 9/8/14.
  */
 public class RulesTest {
-    @Test
-    public void noWinnerTest() {
-        Board board = new Board();
-        Rules rules = new Rules();
+   Board board;
+   Rules rules;
+
+   @Before
+   public void setUp(){
+       this.board = new Board();
+       this.rules = new Rules();
+   }
+   @After
+   public void tearDown() {
+       this.board = null;
+       this.rules = null;
+   }
+   @Test
+   public void noWinnerTest() {
         assertFalse("Empty grid returns false", rules.winner(board.makeSolutions()));
-    }
-    @Test
-    public void winnerRowTest() {
-        Board board = new Board();
-        Rules rules =  new Rules();
+   }
+   @Test
+   public void winnerRowTest() {
         board.setCell('X', 0);
         board.setCell('X', 1);
         board.setCell('X', 2);
         assertTrue("3 in a row returns true", rules.winner(board.makeSolutions()));
-    }
-    @Test
-    public void winnerColTest() {
-        Board board = new Board();
-        Rules rules =  new Rules();
+   }
+   @Test
+   public void winnerColTest() {
         board.setCell('X', 0);
         board.setCell('X', 3);
         board.setCell('X', 6);
         assertTrue("3 in a column return true", rules.winner(board.makeSolutions()));
-    }
+   }
     @Test
     public void drawTest() {
-        Board board = new Board();
-        Rules rules = new Rules();
         board.setCell('X', 0);
         board.setCell('O', 1);
         board.setCell('X', 2);
@@ -50,8 +57,6 @@ public class RulesTest {
     }
     @Test
     public void noDrawTest() {
-        Board board = new Board();
-        Rules rules = new Rules();
         assertFalse("Returns false when there is not a draw", rules.draw(board.getEmpty()));
     }
 }
