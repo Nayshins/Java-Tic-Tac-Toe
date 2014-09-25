e5428536d5aab2cc3a85e80f3c30ed079e372c3b
Jake Nations <jnations@8thlight.com>
2014-09-25 10:17:26 -0500

    Cleaning Engine

--- src/main/me/jakenations/Main.java
+++ src/main/me/jakenations/Main.java
@@ -3,17 +3,6 @@ package me.jakenations;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-	    BoardFactory boardFactory = new BoardFactory();
-        RulesFactory rulesFactory = new RulesFactory();
-        PlayerFactory playerFactory = new PlayerFactory();
 
-            int gameType = 1;
-            Board board = boardFactory.createBoard(gameType);
-            Rules rules = rulesFactory.createRules(gameType, board);
-
-            int player1Input = 1;
-            Player player1 = playerFactory.createPlayer(player1Input, 'X', board, rules);
-            int player2Input = 2;
-            Player player2 = playerFactory.createPlayer(player2Input, 'O', board, rules);
     }
 }
